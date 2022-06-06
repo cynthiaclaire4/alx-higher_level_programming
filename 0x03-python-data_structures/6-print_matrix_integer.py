9115b4a03ca0c9535623df473bf4afd47379aa16
cynthiaclaire4 <you@example.com>
2022-06-06 20:08:13 +0100

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for idx, col in enumerate(row):
+            print("{:d}".format(col), end="")
+            if idx < len(row) - 1:
+                print("{}".format(" "), end="")
+        print()
