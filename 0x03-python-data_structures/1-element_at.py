8a9f5f6145b399225a13f1c10e670eb4aca3608e
cynthiaclaire4 <you@example.com>
2022-06-06 10:42:44 +0100

    2-replace_in_list.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     lc = len(my_list)
-    if idx < 0 or idx > (lc -1):
-        return(None)
+    if idx < 0 or idx > (lc - 1):
+        return (None)
     else:
-        return(my_list[idx])
+        return (my_list[idx])
