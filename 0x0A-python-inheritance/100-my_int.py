a6f6cab933914848f6451ba565273ee305e6328f
cynthiaclaire4 <you@example.com>
2022-07-04 19:29:02 +0100

    100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-MyInt = __import__('100-my_int').MyInt
+class MyInt(int):
+    """ Class that inherits from class int"""
 
-my_i = MyInt(3)
-print(my_i)
-print(my_i == 3)
-print(my_i != 3)
+    def __eq__(self, other):
+        """ Method that returns != check """
+        return int.__ne__(self, other)
+
+    def __ne__(self, other):
+        """ Method that returns == check """
+        return int.__eq__(self, other)
